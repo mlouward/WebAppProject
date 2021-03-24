a65e368ee97f1da41262b29d7711fe74df32388e
mlouward <mlouward@gmail.com>
2021-03-24 20:30:42 +0100

    Homepage/admin page + css

--- 4videogamequizz/server.js
+++ 4videogamequizz/server.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const MongoClient = require("mongodb").MongoClient;
+const path = require('path');
+
 const app = express()
+const client = new MongoClient("mongodb://localhost:27017");
+
+GetCollection();
+app.listen(3000)
+app.use(express.static(__dirname + '/public'))
+
+async function GetCollection() {
+  await client.connect();
+  const db = client.db('webapp')
+  const images_coll = db.collection('images')
+};
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('index')
 })
 
-app.listen(3000)
+app.get('/admin', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/admin.html'))
+})
