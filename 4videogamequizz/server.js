cc3388c28c236eb63e3cf8b05dbff27d10d49597
mlouward <mlouward@gmail.com>
2021-03-28 21:20:00 +0200

    Added delete opition and check for duplicate images

--- 4videogamequizz/server.js
+++ 4videogamequizz/server.js
@@ -34,11 +34,11 @@ MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true })
           // render using template engine EJS
           res.render('admin.ejs', { questions: questions })
         })
-        .catch(err => console.error(err))
+        .catch(console.error)
     })
 
     // Post to create a new card
-    app.post('/create', (req, res,) => {
+    app.post('/questions', (req, res,) => {
       if (!("image" in req.body && "question" in req.body)) {
         // If wrong data in body
         return res.sendStatus(415);
@@ -54,20 +54,40 @@ MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true })
       const alreadyExists = download(req.body.image, path, _ => console.log('File downloaded'));
 
       // if not, add it into the DB
-      if (!alreadyExists)
+      if (!alreadyExists) {
         images_coll.insertOne({
           img_url: req.body.image,
           name: req.body.question
         })
           .then(_ => {
-            console.log('Inserted new question:');
-            console.log(req.body);
-            res.redirect('/admin')
+            return res.redirect('/admin')
           })
-          .catch(err => console.error(err))
+          .catch(console.error)
+      }
+      else {
+        console.log("test redirect");
+        return res.redirect('/admin');
+      }
+    })
+
+    app.delete('/questions', (req, res) => {
+      const str = 'https?://.*' + req.body.id;
+      console.log(str);
+      images_coll.deleteOne(
+        { img_url: { $regex: str } }
+      ).then(result => {
+        if (result.deletedCount !== 0) {
+          // delete local file
+          const path = `${__dirname}\\public\\images\\${req.body.id}`;
+          fs.unlinkSync(path)
+          // return msg
+          return res.json('card deleted')
+        }
+      })
+        .catch(error => console.error(error))
     })
   })
-  .catch(err => console.error(err))
+  .catch(console.error)
 
 // utility function to download a file if not exists
 // Returns whether file existed or not
