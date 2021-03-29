986d0b9bd923facab6773996e7ee51c88ce89edb
mlouward <mlouward@gmail.com>
2021-03-29 13:32:25 +0200

    Basic playing logic and login/signin

--- public/index.js
+++ public/index.js
@@ -6,7 +6,6 @@ const login = document.getElementById('login-button')
 signin.addEventListener('click', async _ => {
   const uname = document.getElementById('suname').value;
   const pwd = document.getElementById('spwd').value;
-  console.log(JSON.stringify({ uname, pwd }));
   const res = await fetch('/signin', {
     method: 'POST',
     headers: {
@@ -19,20 +18,3 @@ signin.addEventListener('click', async _ => {
     alert(response.error);
   }
 });
-
-login.addEventListener('click', async _ => {
-  const uname = document.getElementById('luname').value;
-  const pwd = document.getElementById('lpwd').value;
-  const res = await fetch('/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ uname, pwd })
-  });
-  console.log(res.status);
-  if ([422, 401].includes(res.status)) {
-    const response = await res.json();
-    alert(response.error);
-  }
-})
